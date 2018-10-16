289c4c7e85b24c774baef961441b3afd478e9f89
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2018-10-16 10:51:47 +0200

    fixed list s3 resource; added bundle resource

--- bucket-contents.tf
+++ bucket-contents.tf
@@ -1,17 +1,17 @@
-resource "aws_s3_bucket_object" "web" {
-  bucket = "${aws_s3_bucket.b.bucket}"
-  key = "index.html"
-  source = "site/src/main/web/index.html"
-  content_type = "text/html"
-  etag = "${md5(file("site/src/main/web/index.html"))}"
-}
-resource "aws_s3_bucket_object" "error" {
-  bucket = "${aws_s3_bucket.b.bucket}"
-  key = "error.html"
-  source = "site/src/main/web/error.html"
-  content_type = "text/html"
-  etag = "${md5(file("site/src/main/web/error.html"))}"
-}
+//resource "aws_s3_bucket_object" "web" {
+//  bucket = "${aws_s3_bucket.b.bucket}"
+//  key = "index.html"
+//  source = "site/src/main/web/index.html"
+//  content_type = "text/html"
+//  etag = "${md5(file("site/src/main/web/index.html"))}"
+//}
+//resource "aws_s3_bucket_object" "error" {
+//  bucket = "${aws_s3_bucket.b.bucket}"
+//  key = "error.html"
+//  source = "site/src/main/web/error.html"
+//  content_type = "text/html"
+//  etag = "${md5(file("site/src/main/web/error.html"))}"
+//}
 variable "web" {
   description = "all files to be copied"
   type = "list"
@@ -23,15 +23,18 @@ variable "web" {
     "logo_bad.png",
     "logo_bad_inv.png"]
 }
-//resource "aws_s3_bucket_object" "webcontent" {
-//  count = "${length(var.web)}"
-//  bucket = "${aws_s3_bucket.b.bucket}"
-//  key = "${element(var.web, count.index)}"
-//  source = "site/src/main/web/${element(var.web, count.index)}"
-//  //content_type = "text/html"
-//  etag = "${md5(file("site/src/main/web/error.html"))}"
-//}
+resource "aws_s3_bucket_object" "webcontent" {
+  count = "${length(var.web)}"
+  bucket = "${aws_s3_bucket.b.bucket}"
+  key = "${element(var.web, count.index)}"
+  source = "site/src/main/web/${element(var.web, count.index)}"
+  //content_type = "text/html"
+  etag = "${md5(file(${format("%s%s","site/src/main/web/",element(var.web, 0))}))}"
+}
 //// the bundle is missing
-output "test-contant" {
-  value = "${concat("a","b")}"
+resource "aws_s3_bucket_object" "bundle" {
+  bucket = "${aws_s3_bucket.b.bucket}"
+  key = "kesselringio.bundle.js"
+  source = "site/build/bundle/kesselringio.bundle.js"
+  etag = "${md5(file("site/build/bundle/kesselringio.bundle.js"))}"
 }
