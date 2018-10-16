0c98c3514c50419b95032fb0f8b32c2975c88cf5
Ingwersaft <Ingwersaft@users.noreply.github.com>
2018-10-16 14:39:52 +0200

    bucket-contents.tf: fixed typo in format usage

--- bucket-contents.tf
+++ bucket-contents.tf
@@ -1,17 +1,3 @@
-//resource "aws_s3_bucket_object" "web" {
-//  bucket = "${aws_s3_bucket.b.bucket}"
-//  key = "index.html"
-//  source = "site/src/main/web/index.html"
-//  content_type = "text/html"
-//  etag = "${md5(file("site/src/main/web/index.html"))}"
-//}
-//resource "aws_s3_bucket_object" "error" {
-//  bucket = "${aws_s3_bucket.b.bucket}"
-//  key = "error.html"
-//  source = "site/src/main/web/error.html"
-//  content_type = "text/html"
-//  etag = "${md5(file("site/src/main/web/error.html"))}"
-//}
 variable "web" {
   description = "all files to be copied"
   type = "list"
@@ -29,7 +15,7 @@ resource "aws_s3_bucket_object" "webcontent" {
   key = "${element(var.web, count.index)}"
   source = "site/src/main/web/${element(var.web, count.index)}"
   //content_type = "text/html"
-  etag = "${md5(file(${format("%s%s","site/src/main/web/",element(var.web, 0))}))}"
+  etag = "${md5(file(format("%s%s","site/src/main/web/",element(var.web, 0))))}"
 }
 //// the bundle is missing
 resource "aws_s3_bucket_object" "bundle" {
