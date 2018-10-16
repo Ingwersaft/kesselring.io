f48c0e1ba066e00845db4ad5a6c9784e4d8110b9
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2018-10-16 10:34:45 +0200

    prepared additional aws s3 bucket resources

--- bucket-contents.tf
+++ bucket-contents.tf
@@ -12,3 +12,26 @@ resource "aws_s3_bucket_object" "error" {
   content_type = "text/html"
   etag = "${md5(file("site/src/main/web/error.html"))}"
 }
+variable "web" {
+  description = "all files to be copied"
+  type = "list"
+  default = [
+    "deutschland_heidelberg_header.jpg",
+    "error.html",
+    "index.html",
+    "index2.html",
+    "logo_bad.png",
+    "logo_bad_inv.png"]
+}
+//resource "aws_s3_bucket_object" "webcontent" {
+//  count = "${length(var.web)}"
+//  bucket = "${aws_s3_bucket.b.bucket}"
+//  key = "${element(var.web, count.index)}"
+//  source = "site/src/main/web/${element(var.web, count.index)}"
+//  //content_type = "text/html"
+//  etag = "${md5(file("site/src/main/web/error.html"))}"
+//}
+//// the bundle is missing
+output "test-contant" {
+  value = "${concat("a","b")}"
+}
