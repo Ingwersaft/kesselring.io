a4f583c9b77bfe1fd193eb15a048ad6dd9647795
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2018-10-16 07:59:46 +0200

    replaced custom nav with bulma css

--- site/src/main/kotlin/io/kesselring/OnLoad.kt
+++ site/src/main/kotlin/io/kesselring/OnLoad.kt
@@ -1,7 +1,49 @@
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.asList
+import kotlin.browser.document
 import kotlin.browser.window
+import kotlin.dom.addClass
+import kotlin.dom.removeClass
 
+lateinit var scrollPosition: String
 fun main(args: Array<String>) {
     window.onload = {
-        println("hello")
+        println("hello there")
+
+        handleNavbarStickinessDesktop()
+        addedNavbarBurberClickFunctionality()
+    }
+}
+
+private fun addedNavbarBurberClickFunctionality() {
+    document.querySelectorAll(".navbar-burger").asList().forEach { node: Node ->
+        node.addEventListener("click", callback = {
+            node as Element
+            val target = node.getAttribute("data-target")?.let {
+                document.getElementById(it)
+            }
+            node.classList.toggle("is-active")
+            target?.classList?.toggle("is-active")
+        })
+    }
+//    document.getElementsByClassName("navbar-burger").asList().firstOrNull()?.let { element ->
+//        println("addint onclick event listener")
+//        element.addEventListener("click", callback = {
+//            println("navbar-burger clicked!")
+//        })
+//    }
+}
+
+private fun handleNavbarStickinessDesktop() {
+    window.onscroll = {
+        document.getElementsByClassName("navbar").asList().firstOrNull()?.let {
+            val navTopPosition = it.getBoundingClientRect().top
+            if (navTopPosition > 0) {
+                it.removeClass("is-fixed-top")
+            } else {
+                it.addClass("is-fixed-top")
+            }
+        }
     }
 }
