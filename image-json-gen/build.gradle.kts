96ea8cf871d63cf06ea34c295a464e9f84d8eb99
Ingwersaft <Ingwersaft@users.noreply.github.com>
2019-09-26 12:26:50 +0200

    added distribution invalidation

--- image-json-gen/build.gradle.kts
+++ image-json-gen/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 }
 dependencies {
     val s3_version: String by project
+    val aws_version: String by project
     compile(kotlin("stdlib-jdk8"))
+    compile("com.amazonaws:aws-java-sdk-cloudfront:$aws_version")
     compile("com.amazonaws:aws-java-sdk-s3:$s3_version")
 }
--- image-json-gen/src/main/kotlin/Main.kt
+++ image-json-gen/src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 import com.amazonaws.regions.Regions
+import com.amazonaws.services.cloudfront.AmazonCloudFrontClientBuilder
+import com.amazonaws.services.cloudfront.model.CreateInvalidationRequest
+import com.amazonaws.services.cloudfront.model.InvalidationBatch
+import com.amazonaws.services.cloudfront.model.Paths
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.File
+import java.time.LocalDateTime
 
 val s3 by lazy {
     AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build()
@@ -8,16 +14,32 @@ val s3 by lazy {
 val jackson by lazy {
     ObjectMapper()
 }
+val cloudfront by lazy {
+    AmazonCloudFrontClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build()
+}
 
 fun main(args: Array<String>) {
     val bucketName = "kesselring.io"
     val prefix = "bierschmidts/"
+    val targetName = "keys.json"
     val keys = s3.listObjects(bucketName, prefix).objectSummaries
         .filterNot { it.key == prefix }
+        .filterNot { it.key.endsWith(targetName) }
         .map {
             it.key
         }
 
-    s3.putObject(bucketName, "${prefix}keys.json", jackson.writeValueAsString(keys))
+    s3.putObject(bucketName, "$prefix$targetName", jackson.writeValueAsString(keys))
     println("written keys.json with ${keys.size} entries")
+
+    val distId = File("${System.getProperty("user.home")}${File.separator}.kesselring-io-dist.txt").readText().trim()
+    println("distId=$distId")
+    cloudfront.createInvalidation(
+        CreateInvalidationRequest(
+            distId,
+            InvalidationBatch(Paths().withItems("/$prefix$targetName").withQuantity(1), LocalDateTime.now().toString())
+        )
+    ).let {
+        println("result= $it")
+    }
 }
